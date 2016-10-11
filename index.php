f13b3453c126a11878fee57146e78fd1bb407282
ItalianApkDownloader <apkitaliandownloader@gmail.com>
2016-10-11 15:46:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -49,6 +49,30 @@ if(strpos($message, '/levelpass ') === 0)
 	}
 }
 
+if(strpos($message, '/info ') === 0)
+{
+	$userName = "RobTop";
+	$data = getUserInfo($userName);
+	$userInfo = "This are all the info for the user : ".$data ["name"]."\n";
+	$userInfo = $userInfo."AccountID = ".$data["accountID"]."\n";
+	$userInfo = $userInfo."UserID = ".$data["userID"]."\n";
+	$userInfo = $userInfo."Coins = ".$data["coins"]."\n";
+	$userInfo = $userInfo."User Coins = ".$data["userCoins"]."\n";
+	$userInfo = $userInfo."Demons = ".$data["demons"]."\n";
+	$userInfo = $userInfo."Creator Points = ".$data["creatorPoints"]."\n";
+	$userInfo = $userInfo."Stars = ".$data["stars"]."\n";
+	$userInfo = $userInfo."Youtube channel = ".$data["ytLink"]."\n";
+	
+	
+	
+	$temp = explode("/info", $message);
+	$word = getPassword($temp[1]);
+	sendMessage ($chatId,$userInfo);
+}
+
+
+
+
 
 
 
@@ -181,5 +205,77 @@ $lol = implode($temp);
 
 }
  
+function getUserInfo($userName){
+	$url = 'http://boomlings.com/database/getGJUsers20.php';
+    $data = array('gameVersion' => '20' ,'binaryVersion' => '29' ,'str' => $userName ,'total' => '0' ,'page' => '0' ,'secret' => 'Wmfd2893gb7' );
+
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data),
+        ),
+    );
+
+
+      $context  = stream_context_create($options);
+      $result = file_get_contents($url, false, $context);
+
+      $temp1 = explode("#",$result);
+      $temp2 = explode("|",$temp1[0]);
+      
+      
+      $name = getDato($temp2[0],1);
+      $accID = getDato($temp2[0],16);
+      $userID = getDato($temp2[0],2);
+      
+      
+      $url = 'http://boomlings.com/database/getGJUserInfo20.php';
+    $data = array('gameVersion' => '20' ,'binaryVersion' => '29' ,'udid' => 'S73130148776145348' ,'targetAccountID' => $accID ,'secret' => 'Wmfd2893gb7' );
+
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data),
+        ),
+    );
+
+
+      $context  = stream_context_create($options);
+      $result = file_get_contents($url, false, $context);
+      
+      
+      $coins = getDato($result,13);
+      $ucoins = getDato($result,17);
+      $demons = getDato($result,4);
+      $creatorP = getDato($result,8);
+      $stars = getDato($result,3);
+    	if(getDato($result,20)!= '')
+      		$ytLink = "https://www.youtube.com/channel/".getDato($result,20);
+      
+      
+      
+      
+
+	$data = array('userName' => $name ,'accountID' => $accID , 'userID' => $userID ,'coins' => $coins , 'userCoins' => $ucoins , 'demons' => $demons , 'creatorPoints' => $creatorPoints, 'stars' => $stars, 'ytLink' => $ytLink);
+
+	return $data;
+    
+}
+
+
+
+
+function getDato($value,$number)
+{
+    $data = explode(":",$value);
+   	for($k = 0;$k < count($data);$k= $k+2){
+    	if($data[$k]==$number){
+        	return $data[$k+1];
+        }
+    }
+    
+}
 ?>
 
