678332a593557aaa73cdea376ed9040a77163d35
ItalianApkDownloader <apkitaliandownloader@gmail.com>
2016-10-11 10:22:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -83,30 +83,7 @@ function decode($password)
     return base64_encode ($lol);
     
 }
-function decode($password)
-{
- $decode = base64_decode ($password);
-    $array = str_split($decode);
-    $xor = array(51,55,53,50,54);
-    $key = 0;
-    $k = 0;
-    $cont = 0;
-    foreach ($array as &$value) {
-       $parola[$cont] = ord($value) ^ $xor[$k];
-       if($k != 4){
-            $k++;
-       }else{
-            $k = 0;
-       }
-       $cont++;
-    }
-    for($k = 0; $k < count($parola); $k++){
-        $temp[$k] = chr($parola[$k]);
-    }
-    $lol = implode($temp);
-    
-    return $lol;
-}
+
 function getPassword($levelID){
     
     $url = 'http://boomlings.com/database/downloadGJLevel21.php';
