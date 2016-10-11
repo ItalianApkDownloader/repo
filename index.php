9e2b0bcf9692678152fe0c872d9063dddd5ee3f7
ItalianApkDownloader <apkitaliandownloader@gmail.com>
2016-10-11 02:11:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
 <?php
+ 
+$botToken = "58164031:AAFV0nFXU7Mfla5ro5xp-tyXrd05d1g2pkM";
+$website = "https://api.telegram.org/bot".$botToken;
+ 
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
+ 
+ 
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+ 
+ 
+switch($message) {
+       
+        case "/test":
+                sendMessage($chatId, "test");
+                break;
+        case "/hi":
+                sendMessage($chatId, "hi there!");
+                break;
+        default:
+                sendMessage($chatId, "default");
+       
+}
+ 
+function sendMessage ($chatId, $message) {
+       
+        $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
+        file_get_contents($url);
+       
+}
+ 
+ 
+ 
+ 
+ 
+?>
+
+/*
+<?php
 
 /*
 * This file is part of GeeksWeb Bot (GWB).
@@ -113,3 +153,4 @@ function encode($password)
     return base64_encode ($lol);
     
 }
+*/
